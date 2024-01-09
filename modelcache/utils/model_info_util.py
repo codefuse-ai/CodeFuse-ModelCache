749fe96c94e70a5fff093c77b1941e11ae7da69a
fuhui <hongen.phe@antgroup.com>
2024-01-09 14:42:27 +0800

    add model partition formal func

--- modelcache/utils/model_info_util.py
+++ modelcache/utils/model_info_util.py
@@ -31,3 +31,23 @@ def model_partition_save(model, system_prompt):
     resp = cache.data_manager.save_model_partitio_info(system_prompt, model, version)
     print('insert_resp: {}'.format(resp))
     return version
+
+
+def get_model_partition_formal(system_prompt, model, query_list, partition_info_dict):
+    system_content = None
+    res_list = list()
+    for content_dict in query_list:
+        role = content_dict['role']
+        if role == 'system':
+            system_content = content_dict['content']
+        else:
+            res_list.append(content_dict)
+
+    model_system_key = model + '_' + system_content
+    if model_system_key not in partition_info_dict:
+        partition_version = model_partition_save(model, system_prompt)
+        partition_info_dict[model_system_key] = partition_version
+        return {'partition_version': partition_version, 'query_list': res_list, 'partition_exist': False}
+    else:
+        partition_version = partition_info_dict.get(model_system_key)
+        return {'partition_version': partition_version, 'query_list': res_list, 'partition_exist': True}
